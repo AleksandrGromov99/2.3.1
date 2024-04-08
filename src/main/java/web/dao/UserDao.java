bbedc1543b9fead39284a8cb3ce859c9d7d3f968
AleksandrGromov99 <alegro99@mail.ru>
2024-04-08 14:01:49 +0300

    test

---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,19 @@
+package web.dao;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+   public List<User> getUserList();
+
+   public User getUser(int id);
+
+   public void saveUser(User user);
+
+   public void deleteUser(int id);
+
+   public void updateUser(User user);
+}
+
---
+++ src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,54 @@
+package web.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import web.dao.UserDao;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class UserServiceImpl implements UserService {
+
+    private UserDao userDao;
+
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> getUserList() {
+        return userDao.getUserList();
+    }
+
+    @Transactional
+    @Override
+    public void saveUser(User user) {
+        userDao.saveUser(user);
+    }
+
+    @Transactional
+    @Override
+    public void updateUser(User user){
+        userDao.updateUser(user);
+    }
+    @Transactional(readOnly = true)
+    @Override
+    public User getUser(int id){
+        return userDao.getUser(id);
+    }
+
+    public void deleteUser(int id){
+        userDao.deleteUser(id);
+    }
+}
